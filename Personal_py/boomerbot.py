f670846ab0b6aa114e93ea7f280c1823f425c8bf
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-06-28 17:51:52 +0700

    basic telegram bot
    This script only basic of how your telegram bot will replay. still need lot to learn

---
+++ Personal_py/boomerbot.py
@@ -0,0 +1,49 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Welcome to my bot.")
+
+def aboutme(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I am a bot. Nice to meet you!")
+
+def hello(update, context):
+    message_text = update.message.text.lower()
+    if 'hello' in message_text:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello there!")
+
+def handle_dm2(update, context):
+    # Retrieve the message text and sender's ID
+    message_text = update.message.text
+    sender_id = update.message.from_user.id
+
+    # Send a response to the DM sender
+    response_text = f"Thank you for reaching out! Your message: {message_text}"
+    context.bot.send_message(chat_id=sender_id, text=response_text)
+
+def handle_dm(update, context):
+    user_id = update.message.from_user.id
+    message_text = update.message.text
+
+    reply_text = "Thanks for reaching out! I will check your message shortly."
+
+    context.bot.send_message(chat_id=user_id, text=reply_text)
+
+def main():
+    token = "YOUR_TOKEN_HERE"  # Replace with your actual bot token
+    bot = telegram.Bot(token=token)
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler("start", start)
+    aboutme_handler = CommandHandler("aboutme", aboutme)
+    hello_handler = MessageHandler(Filters.text & (~Filters.command) & Filters.regex(r'(?i)hello'), hello)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(aboutme_handler)
+    dispatcher.add_handler(hello_handler)
+
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
