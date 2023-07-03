45e13ec2ffb5f0d450b13681c816e3a6c9733272
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-03 21:49:45 +0700

    Add files via upload
    #The script prompts the user to enter a password with a length between 6 and 12 characters. It also provides an option to enter '0' to exit the program.
    
    - If the user enters '0', the script displays an exit message and terminates.
    
    - If the user enters a password within the allowed length range, the script calculates the number of possible combinations for that password based on the unique characters present in it. This represents the total number of attempts needed to crack the password.
    
    - The script estimates the time it would take to crack the password based on the assumption that one million attempts can be made per second.
    
    - The estimated crack time is then displayed in a readable format, showing the time in years, months, weeks, days, hours, minutes, and seconds.
    
    - The script repeats the process, allowing the user to enter additional passwords or exit the program by entering '0'.
    
    
    The script helps users understand password strength and the importance of choosing strong, unique passwords. It estimates crack time based on password length and character set, giving users a sense of their password's security. Note: It may not accurately estimate crack time for weak passwords like "123456". Consider implementing stricter complexity rules for improved accuracy.

---
+++ Personal_py/cracker.py
@@ -0,0 +1,49 @@
+import math
+
+def calculate_crack_time(password_length, character_space, attempts_per_second=10**6):
+    if password_length == 0:
+        return "instantly"
+    possibilities = character_space ** password_length
+    seconds = possibilities / attempts_per_second
+    time = seconds_to_readable_time(seconds)
+    return time
+
+def seconds_to_readable_time(seconds):
+    intervals = (
+        ('years', 31536000),  # 60 seconds * 60 minutes * 24 hours * 365 days
+        ('months', 2592000),  # 60 seconds * 60 minutes * 24 hours * 30 days
+        ('weeks', 604800),    # 60 seconds * 60 minutes * 24 hours * 7 days
+        ('days', 86400),      # 60 seconds * 60 minutes * 24 hours
+        ('hours', 3600),      # 60 seconds * 60 minutes
+        ('minutes', 60),
+        ('seconds', 1)
+    )
+
+    result = []
+
+    for name, count in intervals:
+        value = math.floor(seconds / count)
+        if value:
+            seconds -= value * count
+            result.append(f"{value} {name}")
+
+    return ', '.join(result)
+
+# Main program
+if __name__ == "__main__":
+    while True:
+        password = input("Enter the password (6-12 characters) (enter '0' to exit): ")
+
+        if password == '0':
+            print("Exiting the program...")
+            break
+
+        password_length = len(password)
+
+        if password_length < 6 or password_length > 12:
+            print("Password length should be between 6 and 12 characters.")
+            continue
+
+        character_space = len(set(password))
+        crack_time = calculate_crack_time(password_length, character_space)
+        print("Estimated crack time:", crack_time)
