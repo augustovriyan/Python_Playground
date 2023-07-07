bf044fc586feb48b75b0c02f2dcbb114ee31aa98
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-07 20:46:24 +0700

    stopwatch.py
    Stopwatch Script with Running Time Display
    
    This Python script implements a stopwatch functionality that displays the elapsed time while it is in progress. It allows you to start the stopwatch by pressing Enter and stops when you press Enter again. The elapsed time is continuously updated and shown on the same line. Additionally, a space is added between the elapsed time stamp and the prompt to stop the stopwatch for better readability.
    
    Features:
    - Start the stopwatch by pressing Enter
    - Displays the running elapsed time in the format MM:SS.milliseconds
    - Stops the stopwatch and shows the final elapsed time when Enter is pressed again
    - Adds a space between the elapsed time stamp and the prompt to stop the stopwatch for improved formatting
    
    Usage:
    1. Run the script.
    2. Press Enter to start the stopwatch.
    3. The stopwatch will start running and display the elapsed time on the same line.
    4. Press Enter to stop the stopwatch.
    5. The final elapsed time will be displayed.
    
    Feel free to adjust the sleep duration in the script to control the frequency of updating the elapsed time display.
    
    Author: Argus1956

--- Personal_py/stopwatch.py
+++ Personal_py/stopwatch.py
@@ -1,18 +1,33 @@
 import time
+import threading
 
 def stopwatch():
-    input("Press Enter to start the stopwatch...")
-    start_time = time.time()
-    print("Stopwatch started.")
+    def print_elapsed_time():
+        while not stop_event.is_set():
+            elapsed_time = time.time() - start_time
+            minutes = int(elapsed_time // 60)
+            seconds = int(elapsed_time % 60)
+            milliseconds = int((elapsed_time % 1) * 1000)
+            print(f"\rElapsed time: {minutes:02d}:{seconds:02d}.{milliseconds:03d}", end='', flush=True)
+            time.sleep(0.01)  # Adjust the sleep duration as needed
 
-    input("Press Enter to stop the stopwatch...")
+    input("Press Enter to start the stopwatch...")
+    print("Stopwatch started.")
+    stop_event = threading.Event()
+
+    start_time = time.time()
+    timer_thread = threading.Thread(target=print_elapsed_time)
+    timer_thread.start()
+
+    input("\n\nPress Enter to stop the stopwatch...")
+    stop_event.set()
+
     end_time = time.time()
     elapsed_time = end_time - start_time
-
     minutes = int(elapsed_time // 60)
     seconds = int(elapsed_time % 60)
     milliseconds = int((elapsed_time % 1) * 1000)
 
-    print(f"Elapsed time: {minutes:02d}:{seconds:02d}.{milliseconds:03d}")
+    print(f"\nElapsed time: {minutes:02d}:{seconds:02d}.{milliseconds:03d}")
 
 stopwatch()
