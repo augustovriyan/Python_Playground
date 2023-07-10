cc3e1e5ed47e2c7ce2d933fdd67c1a5619295985
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-10 19:41:16 +0700

    game.py
    In this updated code, I added a max_y and max_x variables to get the maximum y and x dimensions of the screen. Then, I defined boundary_top, boundary_bottom, boundary_left, and boundary_right to set the boundaries based on the screen dimensions.
    
    Additionally, I included a list of obstacles with their positions. During each character movement, I check for collisions with these obstacles and reset the character's position if a collision occurs.

--- Personal_py/game.py
+++ Personal_py/game.py
@@ -7,9 +7,19 @@ def main(stdscr):
     stdscr.nodelay(1)  # Make getch() non-blocking
     stdscr.timeout(100)  # Set refresh rate to 100 milliseconds
 
-    # Initial character position
+    # Initialize game variables
     character_x = 0
     character_y = 0
+    max_y, max_x = stdscr.getmaxyx()
+
+    # Define boundaries
+    boundary_top = 0
+    boundary_bottom = max_y - 1
+    boundary_left = 0
+    boundary_right = max_x - 1
+
+    # Define obstacles
+    obstacles = [(5, 10), (8, 20), (12, 5)]  # Example obstacle positions
 
     # Game loop
     while True:
@@ -21,20 +31,28 @@ def main(stdscr):
             break
 
         # Move character based on arrow key input
-        if key == curses.KEY_UP:
+        if key == curses.KEY_UP and character_y > boundary_top:
             character_y -= 1
-        elif key == curses.KEY_DOWN:
+        elif key == curses.KEY_DOWN and character_y < boundary_bottom:
             character_y += 1
-        elif key == curses.KEY_LEFT:
+        elif key == curses.KEY_LEFT and character_x > boundary_left:
             character_x -= 1
-        elif key == curses.KEY_RIGHT:
+        elif key == curses.KEY_RIGHT and character_x < boundary_right:
             character_x += 1
+
+        # Check for collision with obstacles
+        if (character_y, character_x) in obstacles:
+            character_y = 0  # Reset character position if collision occurs
 
         # Clear the screen
         stdscr.clear()
 
         # Draw the character at its new position
         stdscr.addch(character_y, character_x, '@')
+
+        # Draw the obstacles
+        for obstacle in obstacles:
+            stdscr.addch(obstacle[0], obstacle[1], '#')
 
         # Refresh the screen
         stdscr.refresh()
