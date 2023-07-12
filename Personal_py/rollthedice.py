011bb51442a96139485cd8d2e16727254e3ed7e0
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-12 18:15:45 +0700

    rollthedice.py
    With this modification, when you run the program, it will display the individual frequencies of each number rolled along with the results and total.

--- Personal_py/rollthedice.py
+++ Personal_py/rollthedice.py
@@ -2,21 +2,28 @@ import random
 
 def roll_dice(num_dice, num_sides):
     rolls = []
+    frequencies = {}  # Dictionary to store the frequencies
+
     for _ in range(num_dice):
         roll = random.randint(1, num_sides)
         rolls.append(roll)
-    return rolls
+
+        # Update the frequencies dictionary
+        frequencies[roll] = frequencies.get(roll, 0) + 1
+
+    return rolls, frequencies
 
 def main():
     num_dice = int(input("Enter the number of dice to roll: "))
     num_sides = int(input("Enter the number of sides on each die: "))
 
-    rolls = roll_dice(num_dice, num_sides)
+    rolls, frequencies = roll_dice(num_dice, num_sides)
     total = sum(rolls)
 
     print("Rolling {} dice with {} sides each...".format(num_dice, num_sides))
     print("Results: {}".format(rolls))
     print("Total: {}".format(total))
+    print("Frequencies: {}".format(frequencies))
 
 if __name__ == "__main__":
     main()
