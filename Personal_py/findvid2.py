fc1015fac1a88b79bd168a6ba83efb2a7a231b2f
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-14 16:36:51 +0700

    findvid2.py
    In this updated code, I've added the tqdm library, which provides a simple way to create progress bars. The tqdm function is applied to the files iterable in the innermost loop, and the progress bar is updated with each iteration.
    
    Make sure you have the tqdm library installed before running this code. You can install it using pip install tqdm.
    
    Now, when you run the code, you will see a progress bar indicating the search progress, displaying the number of files searched and the current file being processed.

--- Personal_py/findvid2.py
+++ Personal_py/findvid2.py
@@ -1,23 +1,22 @@
-#Here's an updated version of the function with loading progress added:
-
 import os
+from tqdm import tqdm
 
 def find_video_files(search_dirs, video_title):
     video_extensions = [".mkv", ".mp4", ".ts", ".webm"]
     num_files_searched = 0
     num_files_matched = 0
+    
     for start_dir in search_dirs:
         for root, dirs, files in os.walk(start_dir):
-            for file in files:
+            for file in tqdm(files, desc="Searching files", unit="file"):
                 num_files_searched += 1
                 if os.path.splitext(file)[1] in video_extensions:
                     if video_title.lower() in file.lower():
                         num_files_matched += 1
                         print(os.path.join(root, file))
-        print(f"Finished searching {start_dir}. {num_files_matched} files matched out of {num_files_searched} searched.")
+    
+    print(f"Finished searching. {num_files_matched} files matched out of {num_files_searched} searched.")
 
 search_dirs = ["E:\\", "F:\\", "G:\\"]  # Enter the drive letters or paths to search
 title_to_search = input("Enter the title of the video to search for: ")
 find_video_files(search_dirs, title_to_search)
-
-#The function now keeps track of the total number of files searched and the number of files that match the video title. After searching each directory, it prints out an update on the number of files matched and the total number of files searched so far. This gives an idea of the progress of the search.
