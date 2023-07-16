f7803acc316aa42032559282a08aded217663bd3
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-16 21:32:07 +0700

    Update tic.py
    In this updated version, the following changes have been made:
    
    Input Validation: The get_player_input function now ensures that the player's move is valid and prompts for a new move if the input is invalid or the chosen position is already occupied.
    
    Computer Opponent: The get_computer_input function has been added to randomly select an available move for the computer player. The function checks for the unoccupied positions on the board and chooses one randomly.
    
    Now, the game allows the player to enter their move (1-9) and ensures it is valid. The computer opponent selects its move randomly. The game also checks for a draw condition and displays a message accordingly.

--- Personal_py/tic.py
+++ Personal_py/tic.py
@@ -10,7 +10,7 @@ def get_player_input():
     while True:
         player_input = input("Enter your move (1-9): ")
         try:
-            player_input = str(player_input)
+            player_input = int(player_input)
             if player_input in range(1, 10):
                 return player_input
         except ValueError:
@@ -22,21 +22,39 @@ def check_winner(board):
             return board[row][0]
         if board[0][row] == board[1][row] == board[2][row] != " ":
             return board[0][row]
-        if board[0][0] == board[1][1] == board[2][2] != " ":
-            return board[0][0]
-        if board[0][2] == board[1][1] == board[2][0] != " ":
-            return board[0][2]
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
     return None
+
+def get_computer_input(board):
+    available_moves = []
+    for i in range(9):
+        if board[i] == " ":
+            available_moves.append(i)
+    return random.choice(available_moves)
 
 def main():
     board = [" "] * 9
     player = "X"
     while True:
         draw_board(board)
-        move = get_player_input()
-        board[int(move) - 1] = player
+        if player == "X":
+            move = get_player_input()
+            while board[move - 1] != " ":
+                print("Invalid move. Please try again.")
+                move = get_player_input()
+        else:
+            move = get_computer_input(board)
+        board[move - 1] = player
         if check_winner(board):
+            draw_board(board)
             print(player, "wins!")
+            break
+        if " " not in board:
+            draw_board(board)
+            print("It's a draw!")
             break
         player = "O" if player == "X" else "X"
 
