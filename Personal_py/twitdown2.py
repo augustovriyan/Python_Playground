c6c71f3d1c43379914d14a3861ab1dc793ffe2da
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-17 10:45:03 +0700

    Update twitdown2.py
    This version adds a main function that provides options for downloading a Twitter video (option 1) or exiting the program (option 2). It prompts the user for the video URL and filename to save, and then calls the download_twitter_video function accordingly. It will continue to prompt for options until the user chooses to exit.

--- Personal_py/twitdown2.py
+++ Personal_py/twitdown2.py
@@ -1,49 +1,54 @@
-# ```python
 import re
 import urllib.request
 import sys
 
-# from setuptools.config._validate_pyproject.formats import url
+def download_twitter_video(url, filename):
+    # set the maximum resolution for the downloaded video (720p)
+    max_resolution = 720
 
+    # download the HTML source code of the tweet's page
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode('utf-8')
 
-def download_twitter_video(url, filename): (
-    'https://twitter.com/MasterAlecsDOM/status/1670163475421712384', 'test_vid1.mp4')
+    # extract the video URL from the HTML source code
+    pattern = re.compile(
+        r'content="https://video.twimg.com/ext_tw_video/\d+/pu/vid/\d+x(\d+)')
+    match = pattern.search(html)
+    if not match:
+        print('Error: could not find video URL in tweet.')
+        return
+    video_url = re.sub(r'\d+x(\d+)', f'{max_resolution}x\\1', match.group(0)[9:])
 
+    # display a download progress bar
+    print('Downloading...', end='')
 
-# set the maximum resolution for the downloaded video (720p)
-max_resolution = 720
+    def report_hook(count, block_size, total_size):
+        progress = count * block_size * 100 / total_size
+        progress_bar_width = 50
+        progress_bar = ('=' * int(progress / 2)).ljust(progress_bar_width)
+        sys.stdout.write('\r[%s] %.1f%%' % (progress_bar, progress))
+        sys.stdout.flush()
 
-# download the HTML source code of the tweet's page
-# with urllib.request.urlopen(url) as response:
-#    html = response.read().decode('utf-8')
+    # download the video
+    urllib.request.urlretrieve(video_url, filename, report_hook)
+    print('\nDownload complete.')
 
-# extract the video URL from the HTML source code
-pattern = re.compile(
-    r'content="https://video.twimg.com/ext_tw_video/\d+/pu/vid/\d+x(\d+)')
-# match = pattern.search(html)
-# if not match:
-print('Error: could not find video URL in tweet.')
-return
-video_url = re.sub(r'\d+x(\d+)', f'{max_resolution}x\\1', match.group(0)[9:])
+def main():
+    print("Options:")
+    print("1. Download Twitter video")
+    print("2. Exit")
 
-# display a download progress bar
-print('Downloading...', end='')
+    while True:
+        option = input("Enter your choice (1 or 2): ")
+        if option == '1':
+            url = input("Enter the Twitter video URL: ")
+            filename = input("Enter the filename to save the video as: ")
+            download_twitter_video(url, filename)
+        elif option == '2':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option. Please try again.")
 
-
-def report_hook(count, block_size, total_size):
-    progress = count * block_size * 100 / total_size
-    progress_bar_width = 50
-    progress_bar = ('=' * int(progress / 2)).ljust(progress_bar_width)
-    sys.stdout.write('\r[%s] %.1f%%' % (progress_bar, progress))
-    sys.stdout.flush()
-
-
-# download the video
-# urllib.request.urlretrieve(video_url, filename, report_hook)
-print('\nDownload complete.')
-
-# example usage: download a Twitter video and save it as "my_video.mp4"
-# download_twitter_video('https://twitter.com/twitter/status/1397992930550578177', 'my_video.mp4')
-# ```
-
-# Let me know if you have any other questions or need further assistance!
+if __name__ == "__main__":
+    main()
