066e766bd58f5f13a72fa64905ff36c84b43c250
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-07-31 23:07:01 +0700

    Update Factorial_Calculator_in_Python.py
    These improvements make the code more readable, maintainable, and user-friendly.

--- Personal_py/Factorial_Calculator_in_Python.py
+++ Personal_py/Factorial_Calculator_in_Python.py
@@ -1,13 +1,25 @@
 def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer.
+
+    Args:
+        n (int): The number for which to calculate the factorial.
+
+    Returns:
+        int: The factorial of the input number.
+
+    Raises:
+        ValueError: If the input number is negative.
+    """
     if n < 0:
         raise ValueError("Factorial is not defined for negative numbers.")
     elif n == 0:
         return 1
     else:
-        result = 1
+        factorial_result = 1
         for i in range(1, n + 1):
-            result *= i
-        return result
+            factorial_result *= i
+        return factorial_result
 
 if __name__ == "__main__":
     try:
@@ -18,3 +30,4 @@ if __name__ == "__main__":
         print(str(e))
     except Exception as e:
         print("An error occurred:", str(e))
+        
