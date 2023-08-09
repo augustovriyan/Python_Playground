2c0220a4873abbb24b5c17e6667bb7dcad038772
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-08-09 20:23:27 +0700

    Update pw_gen.py
    In this version, the generate_password function now includes a parameter use_punctuation that defaults to True. This allows you to easily generate passwords without punctuation characters if needed. The get_password_length function handles user input validation and ensures a valid password length is obtained.
    
    Remember, while generating strong passwords is important, it's also crucial to consider other security aspects like storing and transmitting passwords securely. Additionally, consider integrating this password generation logic into a larger application, if applicable.

--- Personal_py/pw_gen.py
+++ Personal_py/pw_gen.py
@@ -1,12 +1,26 @@
-import random
+import secrets
 import string
 
-def generate_password(length=12):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+def generate_password(length=12, use_punctuation=True):
+    characters = string.ascii_letters + string.digits
+    if use_punctuation:
+        characters += string.punctuation
+
+    password = ''.join(secrets.choice(characters) for _ in range(length))
     return password
 
+def get_password_length():
+    while True:
+        try:
+            password_length = int(input("Enter the desired password length: "))
+            if password_length < 1:
+                print("Password length must be greater than or equal to 1.")
+            else:
+                return password_length
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 if __name__ == "__main__":
-    password_length = int(input("Enter the desired password length: "))
+    password_length = get_password_length()
     password = generate_password(password_length)
     print("Generated password:", password)
