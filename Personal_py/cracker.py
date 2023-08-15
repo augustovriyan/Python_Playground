5da9e858dd0eff0a40e946839edb892ea8549bbd
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-08-15 18:06:52 +0700

    Update cracker.py
    Changes made to the code:
    
    1. Added a separate function `is_strong_password` to evaluate password strength and provide clearer feedback messages about why a password is rejected.
    
    2. Simplified the `seconds_to_readable_time` function to directly return a formatted string without building a list and joining it later.
    
    3. Modified the time interval labels (e.g., 'years' changed to 'year') and added logic to handle pluralization (e.g., '1 year' vs. '2 years').
    
    4. Improved the requirements check by storing individual checks in a dictionary for easier modification and extension.
    
    5. Used the `is_strong_password` function to validate the password and provide appropriate feedback messages.
    
    6. Removed redundant calculations and simplified the main loop.

--- Personal_py/cracker.py
+++ Personal_py/cracker.py
@@ -5,31 +5,51 @@ def calculate_crack_time(password_length, character_space, attempts_per_second=1
         return "instantly"
     possibilities = character_space ** password_length
     seconds = possibilities / attempts_per_second
-    time = seconds_to_readable_time(seconds)
-    return time
+    return seconds_to_readable_time(seconds)
 
 def seconds_to_readable_time(seconds):
-    intervals = (
-        ('years', 31536000),  # 60 seconds * 60 minutes * 24 hours * 365 days
-        ('months', 2592000),  # 60 seconds * 60 minutes * 24 hours * 30 days
-        ('weeks', 604800),    # 60 seconds * 60 minutes * 24 hours * 7 days
-        ('days', 86400),      # 60 seconds * 60 minutes * 24 hours
-        ('hours', 3600),      # 60 seconds * 60 minutes
-        ('minutes', 60),
-        ('seconds', 1)
-    )
+    intervals = [
+        ('year', 31536000),  # 60 seconds * 60 minutes * 24 hours * 365 days
+        ('month', 2592000),  # 60 seconds * 60 minutes * 24 hours * 30 days
+        ('week', 604800),    # 60 seconds * 60 minutes * 24 hours * 7 days
+        ('day', 86400),      # 60 seconds * 60 minutes * 24 hours
+        ('hour', 3600),      # 60 seconds * 60 minutes
+        ('minute', 60),
+        ('second', 1)
+    ]
 
-    result = []
+    time_parts = []
 
     for name, count in intervals:
         value = math.floor(seconds / count)
         if value:
+            time_parts.append(f"{value} {name}{'s' if value > 1 else ''}")
             seconds -= value * count
-            result.append(f"{value} {name}")
 
-    return ', '.join(result)
+    return ', '.join(time_parts)
 
-# Main program
+def is_strong_password(password):
+    length = len(password)
+    character_space = len(set(password))
+
+    if length < 6 or length > 12:
+        return False, "Password length should be between 6 and 12 characters."
+
+    if character_space < 4:
+        return False, "Password should contain at least four different characters."
+
+    requirements = {
+        'uppercase': any(char.isupper() for char in password),
+        'lowercase': any(char.islower() for char in password),
+        'digit': any(char.isdigit() for char in password),
+        'special': any(not char.isalnum() for char in password),
+    }
+
+    if all(requirements.values()):
+        return True, "Strong password"
+    else:
+        return False, "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
+
 if __name__ == "__main__":
     while True:
         password = input("Enter the password (6-12 characters) (enter '0' to exit): ")
@@ -38,27 +58,12 @@ if __name__ == "__main__":
             print("Exiting the program...")
             break
 
-        password_length = len(password)
+        is_strong, message = is_strong_password(password)
 
-        if password_length < 6 or password_length > 12:
-            print("Password length should be between 6 and 12 characters.")
-            continue
-
-        character_space = len(set(password))
-
-        if character_space == 0:
-            print("Password should contain at least one character.")
-            continue
-
-        # Additional password requirements
-        has_uppercase = any(char.isupper() for char in password)
-        has_lowercase = any(char.islower() for char in password)
-        has_digit = any(char.isdigit() for char in password)
-        has_special = any(not char.isalnum() for char in password)
-
-        if not (has_uppercase and has_lowercase and has_digit and has_special):
-            print("Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
-            continue
-
-        crack_time = calculate_crack_time(password_length, character_space)
-        print("Estimated crack time:", crack_time)
+        if is_strong:
+            password_length = len(password)
+            character_space = len(set(password))
+            crack_time = calculate_crack_time(password_length, character_space)
+            print("Estimated crack time:", crack_time)
+        else:
+            print(message)
