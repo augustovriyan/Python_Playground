dbca85a384a994c126dfe9fdc0d3454a31aba9f9
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-08-18 21:55:26 +0700

    Update pw_gen.py
    while this script can generate strong passwords, users should also be encouraged to use password managers to securely store and manage their passwords.

--- Personal_py/pw_gen.py
+++ Personal_py/pw_gen.py
@@ -1,6 +1,9 @@
 import secrets
 import string
 
+# Constants
+MIN_PASSWORD_LENGTH = 8
+
 def generate_password(length=12, use_punctuation=True):
     characters = string.ascii_letters + string.digits
     if use_punctuation:
@@ -12,15 +15,18 @@ def generate_password(length=12, use_punctuation=True):
 def get_password_length():
     while True:
         try:
-            password_length = int(input("Enter the desired password length: "))
-            if password_length < 1:
-                print("Password length must be greater than or equal to 1.")
+            password_length = int(input("Enter the desired password length (minimum {}): ".format(MIN_PASSWORD_LENGTH)))
+            if password_length < MIN_PASSWORD_LENGTH:
+                print("Password length must be greater than or equal to {}.".format(MIN_PASSWORD_LENGTH))
             else:
                 return password_length
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-if __name__ == "__main__":
+def main():
     password_length = get_password_length()
     password = generate_password(password_length)
     print("Generated password:", password)
+
+if __name__ == "__main__":
+    main()
