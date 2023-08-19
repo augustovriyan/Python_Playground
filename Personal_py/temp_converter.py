adcf9b9c489f5086a6e91161aed5c514db01ab42
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-08-19 16:33:20 +0700

    Update temp_converter.py
    These enhancements should make temperature converter script more user-friendly and robus

--- Personal_py/temp_converter.py
+++ Personal_py/temp_converter.py
@@ -1,23 +1,3 @@
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
-
-def celsius_to_kelvin(celsius):
-    return celsius + 273.15
-
-def kelvin_to_celsius(kelvin):
-    return kelvin - 273.15
-
-def fahrenheit_to_kelvin(fahrenheit):
-    celsius = fahrenheit_to_celsius(fahrenheit)
-    return celsius_to_kelvin(celsius)
-
-def kelvin_to_fahrenheit(kelvin):
-    celsius = kelvin_to_celsius(kelvin)
-    return celsius_to_fahrenheit(celsius)
-
 def temperature_converter():
     print("Temperature Converter")
     while True:
@@ -26,30 +6,36 @@ def temperature_converter():
         print("2. Fahrenheit")
         print("3. Kelvin")
         print("0. Exit")
-        choice = int(input("Enter your choice (0-3): "))
+        choice = input("Enter your choice (0-3): ")
 
-        if choice == 0:
-            print("Exiting the program...")
-            break
-        elif choice == 1:
-            celsius = float(input("Enter temperature in Celsius: "))
-            fahrenheit = celsius_to_fahrenheit(celsius)
-            kelvin = celsius_to_kelvin(celsius)
-            print("Temperature in Fahrenheit:", fahrenheit)
-            print("Temperature in Kelvin:", kelvin)
-        elif choice == 2:
-            fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-            celsius = fahrenheit_to_celsius(fahrenheit)
-            kelvin = fahrenheit_to_kelvin(fahrenheit)
-            print("Temperature in Celsius:", celsius)
-            print("Temperature in Kelvin:", kelvin)
-        elif choice == 3:
-            kelvin = float(input("Enter temperature in Kelvin: "))
-            celsius = kelvin_to_celsius(kelvin)
-            fahrenheit = kelvin_to_fahrenheit(kelvin)
-            print("Temperature in Celsius:", celsius)
-            print("Temperature in Fahrenheit:", fahrenheit)
-        else:
-            print("Invalid choice! Please enter a number between 0 and 3.")
+        try:
+            choice = int(choice)
+            if choice == 0:
+                print("Exiting the program...")
+                break
+            elif choice in [1, 2, 3]:
+                temperature = float(input("Enter temperature: "))
+                
+                if choice == 1:
+                    celsius = temperature
+                    fahrenheit = celsius_to_fahrenheit(celsius)
+                    kelvin = celsius_to_kelvin(celsius)
+                elif choice == 2:
+                    fahrenheit = temperature
+                    celsius = fahrenheit_to_celsius(fahrenheit)
+                    kelvin = fahrenheit_to_kelvin(fahrenheit)
+                else:
+                    kelvin = temperature
+                    celsius = kelvin_to_celsius(kelvin)
+                    fahrenheit = kelvin_to_fahrenheit(kelvin)
+                
+                print("Temperature in Celsius:", round(celsius, 2))
+                print("Temperature in Fahrenheit:", round(fahrenheit, 2))
+                print("Temperature in Kelvin:", round(kelvin, 2))
+            else:
+                print("Invalid choice! Please enter a number between 0 and 3.")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
 
-temperature_converter()
+if __name__ == "__main__":
+    temperature_converter()
