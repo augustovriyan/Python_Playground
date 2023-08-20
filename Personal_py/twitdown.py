c07f7c4b3da6cfaee06e83d9b1c507ed66a73733
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-08-20 18:33:34 +0700

    Update twitdown.py
     It's a good practice to separate different functionalities into functions. This improves code readability and makes it easier to maintain and test

--- Personal_py/twitdown.py
+++ Personal_py/twitdown.py
@@ -1,26 +1,20 @@
-#```python
 import re
 import urllib.request
 import sys
 
-def download_twitter_video(url, filename): ('https://twitter.com/MasterAlecsDOM/status/1670163475421712384', 'test_1.mp4')
-    # set the maximum resolution for the downloaded video (720p)
-    max_resolution = 720
-
-    # download the HTML source code of the tweet's page
-    with urllib.request.urlopen(url) as response:
+def get_video_url(tweet_url, max_resolution=720):
+    with urllib.request.urlopen(tweet_url) as response:
         html = response.read().decode('utf-8')
 
-    # extract the video URL from the HTML source code
     pattern = re.compile(r'content="https://video.twimg.com/ext_tw_video/\d+/pu/vid/\d+x(\d+)')
     match = pattern.search(html)
     if not match:
         print('Error: could not find video URL in tweet.')
-        return
+        return None
     video_url = re.sub(r'\d+x(\d+)', f'{max_resolution}x\\1', match.group(0)[9:])
+    return video_url
 
-    # display a download progress bar
-    print('Downloading...', end='')
+def download_video(video_url, filename):
     def report_hook(count, block_size, total_size):
         progress = count * block_size * 100 / total_size
         progress_bar_width = 50
@@ -28,14 +22,14 @@ def download_twitter_video(url, filename): ('https://twitter.com/MasterAlecsDOM/
         sys.stdout.write('\r[%s] %.1f%%' % (progress_bar, progress))
         sys.stdout.flush()
 
-    # download the video
     urllib.request.urlretrieve(video_url, filename, report_hook)
     print('\nDownload complete.')
 
-# example usage: download a Twitter video and save it as "my_video.mp4"
-#download_twitter_video('https://twitter.com/twitter/status/1397992930550578177', 'my_video.mp4')
-#```
-
-#When you run this script, it will show a progress bar that indicates the progress of the download. Once the download is complete, it will display a message saying "Download complete." and the script will exit.
-
-#Note that the progress bar can only be displayed on the command line (i.e. not in a Jupyter notebook or other similar environment). If you're running the script in a terminal, you should see the progress bar updating in real time as the download progresses.
+if __name__ == "__main__":
+    tweet_url = 'https://twitter.com/MasterAlecsDOM/status/1670163475421712384'
+    video_url = get_video_url(tweet_url)
+    
+    if video_url:
+        filename = 'test_1.mp4'
+        print('Downloading...')
+        download_video(video_url, filename)
