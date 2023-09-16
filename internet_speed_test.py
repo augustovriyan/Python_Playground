3e7d30270f05cc0b3f3e9865805560cd1b2ddec2
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-09-16 17:24:07 +0700

    Update internet_speed_test.py

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -10,7 +10,10 @@ def test_internet_speed():
     download_speed = st.download() / 1_000_000  # Convert to Mbps
     upload_speed = st.upload() / 1_000_000  # Convert to Mbps
     
+    print("Testing download speed...")
     print(f"Download Speed: {download_speed:.2f} Mbps")
+    
+    print("\nTesting upload speed...")
     print(f"Upload Speed: {upload_speed:.2f} Mbps")
 
 if __name__ == "__main__":
