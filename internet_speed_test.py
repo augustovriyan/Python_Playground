eb64a950afc16a1e42aefe95a6f980b1769e1263
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-09-17 22:09:40 +0700

    Update internet_speed_test.py

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -1,18 +1,21 @@
 import speedtest
 
 def test_internet_speed():
+    # Create a Speedtest object
     st = speedtest.Speedtest()
     
-    # Get the best server
+    # Find the best server for testing
     st.get_best_server()
     
     # Perform download and upload speed tests
     download_speed = st.download() / 1_000_000  # Convert to Mbps
     upload_speed = st.upload() / 1_000_000  # Convert to Mbps
     
+    # Print download speed results
     print("Testing download speed...")
     print(f"Download Speed: {download_speed:.2f} Mbps")
     
+    # Print upload speed results
     print("\nTesting upload speed...")
     print(f"Upload Speed: {upload_speed:.2f} Mbps")
 
