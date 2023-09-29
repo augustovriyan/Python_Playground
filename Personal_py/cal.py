947727e6f62512ab8d83e69a7f4e0b0f60d3688f
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-09-29 16:22:02 +0700

    Update cal.py

--- Personal_py/cal.py
+++ Personal_py/cal.py
@@ -12,17 +12,27 @@ def divide(x, y):
         return "Error! Division by zero."
     return x / y
 
+def exponentiate(x, y):
+    return x ** y
+
+def modulus(x, y):
+    if y == 0:
+        return "Error! Modulus by zero."
+    return x % y
+
 print("Calculator")
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Exponentiate")
+print("6. Modulus")
 
 while True:
-    choice = input("Enter choice (1-4): ")
+    choice = input("Enter choice (1-6): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5', '6'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -38,6 +48,14 @@ while True:
                 print(result)
             else:
                 print(f"{num1} / {num2} = {result}")
+        elif choice == '5':
+            print(f"{num1} ** {num2} = {exponentiate(num1, num2)}")
+        elif choice == '6':
+            result = modulus(num1, num2)
+            if isinstance(result, str):
+                print(result)
+            else:
+                print(f"{num1} % {num2} = {result}")
         break
     else:
         print("Invalid input. Please try again.")
