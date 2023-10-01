894dc32c754fc02bf09176d322151da82fe78a2d
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-10-01 14:08:18 +0700

    Update IG_Downloader.py

--- Personal_py/IG_Downloader.py
+++ Personal_py/IG_Downloader.py
@@ -1,39 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-# URL of the Instagram post or story
-post_url = "https://www.instagram.com/p/ABC123/"
+def download_instagram_media(post_url, download_dir="./"):
+    try:
+        # Validate the Instagram URL
+        if not re.match(r"https://www\.instagram\.com/p/[\w-]+/$", post_url):
+            print("Invalid Instagram URL. Please provide a valid URL.")
+            return
 
-try:
-    # Get the source page of the Instagram post or story
-    response = requests.get(post_url)
-    response.raise_for_status()  # Check for any HTTP errors
-
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Find the video or image element
-    media_element = soup.find("meta", property="og:video") or soup.find("meta", property="og:image")
-
-    if media_element:
-        media_url = media_element.get("content")
-
-        # Determine the file extension based on the media URL
-        file_extension = "mp4" if "video" in media_url else "jpg"
-
-        # Download the media to the specified directory
-        response = requests.get(media_url)
+        # Get the source page of the Instagram post or story
+        response = requests.get(post_url)
         response.raise_for_status()  # Check for any HTTP errors
 
-        # Save the downloaded media with a descriptive filename
-        filename = f"downloaded_media.{file_extension}"
-        with open(filename, "wb") as outfile:
-            outfile.write(response.content)
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        print(f"Media successfully downloaded as '{filename}'")
+        # Find the video or image element
+        media_element = soup.find("meta", property="og:video") or soup.find("meta", property="og:image")
 
-    else:
-        print("No media found on the given URL")
+        if media_element:
+            media_url = media_element.get("content")
 
-except requests.exceptions.RequestException as e:
-    print("Error occurred during the download:", str(e))
+            # Determine the file extension based on the media URL
+            file_extension = "mp4" if "video" in media_url else "jpg"
+
+            # Download the media to the specified directory
+            response = requests.get(media_url)
+            response.raise_for_status()  # Check for any HTTP errors
+
+            # Construct the full file path
+            filename = f"{download_dir}/downloaded_media.{file_extension}"
+
+            # Save the downloaded media with a descriptive filename
+            with open(filename, "wb") as outfile:
+                outfile.write(response.content)
+
+            print(f"Media successfully downloaded as '{filename}'")
+
+        else:
+            print("No media found on the given URL")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred during the download:", str(e))
+
+if __name__ == "__main__":
+    post_url = input("Enter the Instagram post URL: ")
+    download_dir = input("Enter the download directory (default is current directory): ")
+    
+    if not download_dir:
+        download_dir = "./"
+
+    download_instagram_media(post_url, download_dir)
