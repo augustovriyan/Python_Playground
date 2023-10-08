23dbfe7436864c1f31bbe458591fcbb5803645d0
Agus Riyanto <45610495+Argus1956@users.noreply.github.com>
2023-10-08 16:54:54 +0700

    Update bmi.py

--- Personal_py/bmi.py
+++ Personal_py/bmi.py
@@ -1,6 +1,6 @@
-def calculate_bmi(weight, height_cm):
+def calculate_bmi(weight_kg, height_cm):
     height_m = height_cm / 100  # Convert height from centimeters to meters
-    return weight / (height_m ** 2)
+    return weight_kg / (height_m ** 2)
 
 def interpret_bmi(bmi):
     if bmi < 18.5:
@@ -14,14 +14,14 @@ def interpret_bmi(bmi):
 
 def get_numeric_input(prompt):
     while True:
-        user_input = input(prompt)
         try:
-            value = float(user_input)
+            value = float(input(prompt))
             return value
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
 
 if __name__ == "__main__":
+    print("BMI Calculator")
     weight = get_numeric_input("Enter your weight in kilograms: ")
     height = get_numeric_input("Enter your height in centimeters: ")
 
