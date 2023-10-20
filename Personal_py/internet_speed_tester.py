53d72c13e35ec378644fbf33fbc96dcf748e626c
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-10-20 18:06:51 +0700

    Update internet_speed_tester.py

--- Personal_py/internet_speed_tester.py
+++ Personal_py/internet_speed_tester.py
@@ -4,15 +4,20 @@ def test_internet_speed():
     # Create a Speedtest client
     st = speedtest.Speedtest()
 
+    # Test download speed
     print("Testing download speed...")
     download_speed = st.download() / 1_000_000  # Convert to Mbps
     print(f"Download speed: {download_speed:.2f} Mbps")
 
+    # Test upload speed
     print("Testing upload speed...")
     upload_speed = st.upload() / 1_000_000  # Convert to Mbps
     print(f"Upload speed: {upload_speed:.2f} Mbps")
 
+    # Get the best server for accurate results
     server = st.get_best_server()
+
+    # Display the best server's information
     print(f"Best server: {server['host']} ({server['country']})")
 
 if __name__ == "__main__":
