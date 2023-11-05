8ae35b98d140e2997aecc99f9eb174d5603a9e7b
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-11-05 15:43:35 +0700

    Update temp_converter.py

--- Personal_py/temp_converter.py
+++ Personal_py/temp_converter.py
@@ -1,21 +1,24 @@
 def temperature_converter():
     print("Temperature Converter")
+
     while True:
         print("\nSelect the temperature unit you want to input:")
         print("1. Celsius")
         print("2. Fahrenheit")
         print("3. Kelvin")
         print("0. Exit")
+
         choice = input("Enter your choice (0-3): ")
 
         try:
             choice = int(choice)
+
             if choice == 0:
                 print("Exiting the program...")
                 break
             elif choice in [1, 2, 3]:
                 temperature = float(input("Enter temperature: "))
-                
+
                 if choice == 1:
                     celsius = temperature
                     fahrenheit = celsius_to_fahrenheit(celsius)
@@ -28,7 +31,7 @@ def temperature_converter():
                     kelvin = temperature
                     celsius = kelvin_to_celsius(kelvin)
                     fahrenheit = kelvin_to_fahrenheit(kelvin)
-                
+
                 print("Temperature in Celsius:", round(celsius, 2))
                 print("Temperature in Fahrenheit:", round(fahrenheit, 2))
                 print("Temperature in Kelvin:", round(kelvin, 2))
