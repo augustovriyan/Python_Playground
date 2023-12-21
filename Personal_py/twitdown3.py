ce3bc97a4f63eadea0a68d51cd7e631966212c73
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-12-21 11:11:07 -0800

    Update twitdown3.py

--- Personal_py/twitdown3.py
+++ Personal_py/twitdown3.py
@@ -1,26 +1,49 @@
 import requests
+import re
 
-# URL of the Twitter video to be downloaded
-video_url = "https://twitter.com/i/status/1670163475421712384"
+def get_random_twitter_video_url():
+    """
+    Fetch a random Twitter video URL.
+    Note: This is a placeholder function and requires additional logic to fetch a random URL dynamically.
+    For simplicity, we'll use a static random video URL for demonstration purposes.
+    """
+    return "https://twitter.com/i/status/1670163475421712384"  # Placeholder for demonstration
 
-try:
-    # Get the source page of the Twitter video
-    response = requests.get(video_url)
-    response.raise_for_status()  # Check for any HTTP errors
+def download_video(video_url, save_path):
+    """
+    Download a video from the provided URL and save it to the specified path.
+    """
+    try:
+        # Fetch the source page of the Twitter video
+        response = requests.get(video_url)
+        response.raise_for_status()  # Check for any HTTP errors
 
-    # Find the video URL with a simple resolution
-    start = response.text.find('https://video.twimg.com/')
-    end = response.text.find('.mp4', start) + 4
-    video_link = response.text[start:end]
+        # Extract the video URL using regular expressions (more robust than string search)
+        match = re.search(r'(https://video.twimg.com[^"]+)', response.text)
+        if not match:
+            print("Video URL not found.")
+            return
 
-    # Download the video to the F:\ directory
-    response = requests.get(video_link)
-    response.raise_for_status()  # Check for any HTTP errors
+        video_link = match.group(0)
 
-    with open(r"F:\twitter_video.mp4", "wb") as outfile:
-        outfile.write(response.content)
+        # Download the video
+        response = requests.get(video_link)
+        response.raise_for_status()  # Check for any HTTP errors
 
-    print("Video successfully downloaded to F:\twitter_video.mp4")
+        with open(save_path, "wb") as outfile:
+            outfile.write(response.content)
 
-except requests.exceptions.RequestException as e:
-    print("Error occurred during the download:", str(e))
+        print(f"Video successfully downloaded to {save_path}")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred during the download:", str(e))
+
+if __name__ == "__main__":
+    # Get a random Twitter video URL (for demonstration purposes)
+    random_video_url = get_random_twitter_video_url()
+
+    # Define the path to save the downloaded video
+    save_path = r"F:\twitter_video.mp4"
+
+    # Download the video
+    download_video(random_video_url, save_path)
