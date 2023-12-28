2ac4704a2b1b1cbecba0df9508921bf0785ad427
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-12-28 00:16:13 -0800

    Update cal.py

--- Personal_py/cal.py
+++ Personal_py/cal.py
@@ -9,27 +9,21 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
-    try:
-        if y == 0:
-            raise ZeroDivisionError("Error! Division by zero.")
-        return x / y
-    except ZeroDivisionError as e:
-        return str(e)
+    if y == 0:
+        raise ValueError("Error! Division by zero.")
+    return x / y
 
 def exponentiate(x, y):
     return x ** y
 
 def modulus(x, y):
-    try:
-        if y == 0:
-            raise ZeroDivisionError("Error! Modulus by zero.")
-        return x % y
-    except ZeroDivisionError as e:
-        return str(e)
+    if y == 0:
+        raise ValueError("Error! Modulus by zero.")
+    return x % y
 
 # Display calculator menu
 def display_menu():
-    print("Calculator")
+    print("\nCalculator Menu")
     print("Select operation:")
     print("1. Add")
     print("2. Subtract")
@@ -41,29 +35,37 @@ def display_menu():
 # Main calculator loop
 while True:
     display_menu()
-    choice = input("Enter choice (1-6): ")
+    choice = input("Enter your choice (1-6): ")
     
     if choice in ('1', '2', '3', '4', '5', '6'):
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
         except ValueError:
-            print("Invalid input. Please enter valid numbers.")
+            print("Invalid input! Please enter valid numbers.")
             continue
 
-        result = None
+        # Perform the selected operation
+        operations = {
+            '1': add,
+            '2': subtract,
+            '3': multiply,
+            '4': divide,
+            '5': exponentiate,
+            '6': modulus
+        }
 
-        # Perform selected operation
-        operations = {'1': add, '2': subtract, '3': multiply, '4': divide, '5': exponentiate, '6': modulus}
         try:
             result = operations[choice](num1, num2)
-            print(f"Result: {num1} {'+'.join(['', '-', '*', '/', '**', '%'][int(choice) - 1])} {num2} = {result}")
+            print(f"\nResult: {num1} {'+'.join(['', '-', '*', '/', '**', '%'][int(choice) - 1])} {num2} = {result}")
+        except ValueError as ve:
+            print(ve)
         except Exception as e:
-            print(e)
+            print(f"An error occurred: {e}")
 
-        # Ask user if they want to continue
-        another_calculation = input("Do you want to perform another calculation? (yes/no): ").lower()
+        # Ask if the user wants to continue
+        another_calculation = input("\nDo you want to perform another calculation? (yes/no): ").lower()
         if another_calculation != 'yes':
             break
     else:
-        print("Invalid input. Please try again.")
+        print("Invalid input! Please select a valid option.")
