920d49c9bed53c3ec37bd2109c639c6066d8c7f4
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-12-30 11:11:25 -0800

    Update stopwatch.py

--- Personal_py/stopwatch.py
+++ Personal_py/stopwatch.py
@@ -8,15 +8,17 @@ class Stopwatch:
         self.timer_thread = None
 
     def _print_elapsed_time(self):
+        """Print elapsed time continuously until the stop event is set."""
         while not self.stop_event.is_set():
             elapsed_time = time.time() - self.start_time
-            minutes = int(elapsed_time // 60)
-            seconds = int(elapsed_time % 60)
+            minutes, seconds = divmod(elapsed_time, 60)
             milliseconds = int((elapsed_time % 1) * 1000)
-            print(f"\rElapsed time: {minutes:02d}:{seconds:02d}.{milliseconds:03d}", end='', flush=True)
-            time.sleep(0.01)  # Adjust the sleep duration as needed
+            
+            print(f"\rElapsed time: {int(minutes):02d}:{int(seconds):02d}.{milliseconds:03d}", end='', flush=True)
+            time.sleep(0.01)  # Adjust the sleep duration if needed
 
     def start(self):
+        """Start the stopwatch."""
         input("Press Enter to start the stopwatch...")
         print("Stopwatch started.")
         self.start_time = time.time()
@@ -24,19 +26,19 @@ class Stopwatch:
         self.timer_thread.start()
 
     def stop(self):
+        """Stop the stopwatch and print the elapsed time."""
         input("\n\nPress Enter to stop the stopwatch...")
         self.stop_event.set()
         self.timer_thread.join()
 
-        end_time = time.time()
-        elapsed_time = end_time - self.start_time
-        minutes = int(elapsed_time // 60)
-        seconds = int(elapsed_time % 60)
+        elapsed_time = time.time() - self.start_time
+        minutes, seconds = divmod(elapsed_time, 60)
         milliseconds = int((elapsed_time % 1) * 1000)
-
-        print(f"\nElapsed time: {minutes:02d}:{seconds:02d}.{milliseconds:03d}")
+        
+        print(f"\nElapsed time: {int(minutes):02d}:{int(seconds):02d}.{milliseconds:03d}")
 
 def main():
+    """Main function to initiate the stopwatch."""
     stopwatch = Stopwatch()
     stopwatch.start()
     stopwatch.stop()
