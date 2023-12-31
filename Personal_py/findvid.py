0cdf269db63d0a5470b02b583e6b45d519c86268
Agus Riyanto <45610495+augustovriyan@users.noreply.github.com>
2023-12-31 03:55:25 -0800

    Update findvid.py

--- Personal_py/findvid.py
+++ Personal_py/findvid.py
@@ -1,15 +1,32 @@
 import os
 
-def find_video_files(search_dirs, video_title):
+def find_video_files(search_directories, target_title):
+    """Search for video files with a specific title within given directories."""
+    
+    # List of video file extensions to consider
     video_extensions = [".mkv", ".mp4", ".ts", ".webm"]
-    for start_dir in search_dirs:
-        for root, dirs, files in os.walk(start_dir):
-            for file in files:
-                if os.path.splitext(file)[1] in video_extensions:
-                    if video_title.lower() in file.lower():
-                        video_path = os.path.join(root, file)
-                        print("Found:", video_path)
+    
+    # Iterate over each start directory provided for the search
+    for start_directory in search_directories:
+        # Walk through the directory tree starting from the given directory
+        for current_root, _, files in os.walk(start_directory):
+            for file_name in files:
+                # Check if the file has a video extension and matches the target title
+                if os.path.splitext(file_name)[1].lower() in video_extensions and target_title.lower() in file_name.lower():
+                    video_path = os.path.join(current_root, file_name)
+                    print("Found:", video_path)
 
-search_dirs = ["E:\\", "F:\\", "G:\\"]  # Enter the drive letters or paths to search
-title_to_search = input("Enter the title of the video to search for: ")
-find_video_files(search_dirs, title_to_search)
+def main():
+    """Main function to initiate the search for video files."""
+    
+    # Directories to search for video files (add or modify paths as needed)
+    search_directories = ["E:\\", "F:\\", "G:\\"]
+    
+    # Prompt user to enter the title of the video to search for
+    title_to_search = input("Enter the title of the video to search for: ")
+    
+    # Call the function to find video files with the specified title in the directories
+    find_video_files(search_directories, title_to_search)
+
+if __name__ == "__main__":
+    main()
